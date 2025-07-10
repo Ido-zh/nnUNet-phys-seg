96e674c4f08c08f39b24d2b2af8810ce3ccbd27a
yidong_tud_linux <y.zhao-8@tudelft.nl>
2025-07-10 14:53:08 +0200

    More augmentation! (Blur)

--- nnunet/training/data_augmentation/get_phys_augmentation.py
+++ nnunet/training/data_augmentation/get_phys_augmentation.py
@@ -88,7 +88,7 @@ def get_moreDA_inv_augmentation(dataloader_train, dataloader_val, patch_size, pa
     # we need to put the color augmentations after the dummy 2d part (if applicable). Otherwise the overloaded color
     # channel gets in the way
     tr_transforms.append(GaussianNoiseTransform(noise_variance=(0, 0.5), p_per_sample=0.3))
-    tr_transforms.append(GaussianBlurTransform((0.5, 1.), different_sigma_per_channel=True, p_per_sample=0.2,
+    tr_transforms.append(GaussianBlurTransform((0.5, 4.), different_sigma_per_channel=True, p_per_sample=0.3,
                                                p_per_channel=0.5))
     tr_transforms.append(BrightnessMultiplicativeTransform(multiplier_range=(0.75, 1.25), p_per_sample=0.15))
 
